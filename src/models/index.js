d1545bd795e50a86aa3fc0d6a5875b8e1a05c9a2
Kyrexy <loldude.bm@gmail.com>
2026-04-12 16:04:07 +0700

    Changed some stuff

--- src/models/index.js
+++ src/models/index.js
@@ -12,9 +12,6 @@ connectionString.pathname += config.database.name;
 mongoose.connect(`${connectionString.toString()}`);
 
 const db = mongoose.connection;
-db.once('open', () => {
-  logger.info('Successfully connected to MongoDB');
-});
 
 const dbExports = {};
 dbExports.db = db;
@@ -31,5 +28,22 @@ fs.readdirSync(__dirname)
     const model = require(path.join(__dirname, file))(mongoose);
     dbExports[model.modelName] = model;
   });
+
+db.once('open', async () => {
+  logger.info('Successfully connected to MongoDB');
+
+  const Prize = mongoose.model('Prize');
+  const count = await Prize.countDocuments();
+  if (count === 0) {
+    await Prize.insertMany([
+      { name: 'Emas 10 gram', Kuota: 1, sisaKuota: 1 },
+      { name: 'Smartphone X', Kuota: 5, sisaKuota: 5 },
+      { name: 'Smartwatch Y', Kuota: 10, sisaKuota: 10 },
+      { name: 'Voucher Rp100.000', Kuota: 100, sisaKuota: 100 },
+      { name: 'Pulsa Rp50.000', Kuota: 500, sisaKuota: 500 },
+    ]);
+    logger.info('Prizes seeded!');
+  }
+});
 
 module.exports = dbExports;
