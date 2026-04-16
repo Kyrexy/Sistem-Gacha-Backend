86c3a06954e47321c11cef7d8baebfeda2686768
Kyrexy <loldude.bm@gmail.com>
2026-04-16 09:18:49 +0700

    blabla

--- src/api/components/gacha/gacha-hadiah.js
+++ src/api/components/gacha/gacha-hadiah.js
@@ -7,12 +7,7 @@ const prizes = [
   { name: 'Emas 10 gram', Kuota: 1, sisaKuota: 1, probability: 0.005 },
   { name: 'Smartphone X', Kuota: 5, sisaKuota: 5, probability: 0.02 },
   { name: 'Smartwatch Y', Kuota: 10, sisaKuota: 10, probability: 0.04 },
-  {
-    name: 'Voucher Rp100.000',
-    Kuota: 100,
-    sisaKuota: 100,
-    probability: 0.15,
-  },
+  { name: 'Voucher Rp100.000', Kuota: 100, sisaKuota: 100, probability: 0.15 },
   { name: 'Pulsa Rp50.000', Kuota: 500, sisaKuota: 500, probability: 0.3 },
 ];
 
